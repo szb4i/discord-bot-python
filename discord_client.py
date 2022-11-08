8e9932e13234038382fe284209da889d3d44d6b4
szbai <szabolcs.nagy.hu@gmail.com>
2022-11-08 21:46:31 +0100

    added discord_client.py

---
+++ discord_client.py
@@ -0,0 +1,28 @@
+import discord
+from binance.enums import *
+from configuration import *
+from execute import Execute
+
+class DiscordClient(discord.Client):
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
+        self.execute = Execute()
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        symbol = TRADE_SYMBOL
+        side = SIDE_BUY
+        direction = 'long'
+        if SIDE_BUY == side and 'long' == direction:
+            self.execute.open_long(symbol)
+        elif SIDE_SELL == side and 'long' == direction:
+            self.execute.close_long(symbol)
+        elif SIDE_SELL == side and 'short' == direction:
+            self.execute.open_short(symbol)
+        elif SIDE_BUY == side and 'short' == direction:
+            self.execute.close_short(symbol)
